66d084faf14eda88e279442c43740ca05aab63a2
Juani <juanignacio0298@gmail.com>
2022-03-29 16:37:44 -0300

    Update changes

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,42 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id:{
+      type: DataTypes.UUID,
+      allowNull: false,
+      unique: true,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    date: {
+      type: DataTypes.STRING,
+    },
+    rating: {
+      type: DataTypes.STRING,
+    },
+    platforms: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false
+    },
+    background_image: {
+      type: DataTypes.STRING(1234),
+    }
+  }, {
+    timestamps: false,
+    createdAt: false,
+    updatedAt: false
   });
 };
+
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,174 @@
 const { Router } = require('express');
+require("dotenv").config();
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-
+const axios = require('axios');
+const {Videogame, Genre} = require("../db")
 const router = Router();
+const {API_KEY} = process.env;
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+const getApiInfo = async () => {
+    try {
+        //Creo un array vacio donde estaran los juegos. Hago una variable page que sera la que traera los 15 juegos por pagina de la api
+        //Por eso hago un while hasta el 5. Asi me traigo solo 100 juegos y no todos los que ofrece la api
+        let results = [];
+        const queries = [];
+        const pages = [1,2,3,4,5];
+        pages.forEach((page) => {
+            queries.push(axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&page=${page}`));
+        });
+        await Promise.all(queries) 
+            .then((queryResults) => {
+                queryResults.forEach((queryResult) => { 
+                    let response = queryResult.data
+                    results.push(
+                        ...response.results.map((e) => ({
+                            id: e.id,
+                            name: e.name,
+                            background_image: e.background_image,
+                            genres: e.genres.map((g) => `${g.name}`),
+                            rating: e.rating,
+                            platforms: e.platforms.map((p) => `${p.platform.name}`)
+                        }))
+                    )
+                })
+            })
+            .then(() => results)
+            .catch((err) => console.log(err));
+            return results
+    } catch (err) {
+        console.log(err)
+    }
+}
 
+const callDbInfo = async () => {
+    try {
+        return await Videogame.findAll({
+            include:{
+                model: Genre,
+                attributes: ['name'],
+                trough: {
+                    attributes: [],
+                }
+            },
+        })
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const callAllVideogames = async () => {
+    const apiData = await getApiInfo();
+    const dbData = await callDbInfo();
+    const dataTotal = apiData.concat(dbData);
+    return dataTotal;
+}
+    
+router.get('/videogames', async (req,res) => {
+    // ME GUARDO EL NAME QUE ME LLEGA POR QUERY PARA USARLO CUANDO LO INDIQUE
+    const {name} = req.query;
+    try {
+        let allVideogames = await callAllVideogames();
+        if(name){
+            let videogameName = allVideogames.filter(game => game.name.toLowerCase().includes(name.toLowerCase()));
+            videogameName.length ? 
+            res.status(200).send(videogameName) : 
+            res.status(404).send('Este videogame no existe');
+        }else{
+            res.status(200).send(allVideogames);
+        }
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+router.get('/genres', async (req,res) => {
+    try {
+        const genre = await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`);
+        const {results} = genre.data;
+        //Itero cada uno de los resultados para extraer las propiedades name, si existe no la creo y si no existe la creo
+        for (let i = 0; i < results.length; i++) {
+            const {name} = results[i]
+            await Genre.findOrCreate({
+                where: {name: name}
+            })
+        }
+        res.status(200).json(await Genre.findAll());
+    } catch (error) {
+        
+    }
+})
+
+router.get('/videogame/:idVideogame', async (req,res) => {
+    const id =  req.params.idVideogame;
+    try {
+        allVideogames = await callAllVideogames();
+        if(id){
+            let gameId = await allVideogames.filter(g=> g.id == id);// filtro por id mis juegos de la db y los 100 juegos de la api 
+            gameId.length?
+            res.status(200).json(gameId) : // si lo encuentra lo envia
+            res.status(404).send('Videogame Not Found') // sino envia un mensaje de error
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.post('/videogame', async (req,res) => {
+    try {
+        const { name, background_image, description, date, rating, platforms, genres } = req.body;
+        const newVideogame = await Videogame.create({
+            name,
+            background_image,
+            description,
+            platforms,
+            rating,
+            date
+        });
+        const genreDb = await Genre.findAll({
+            where:{ name: genres}
+         });
+         newVideogame.addGenre(genreDb);
+         res.send('Videogame Successfully Created');
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.get('/platforms', async(req,res) =>{
+    try {
+        const games = await callAllVideogames(); // me traigo los 100 juegos de la api y los de mi db
+        const everyPlatforms = []; 
+        games.forEach(g => { // recorro los juegos y pusheo a un array todos las plataformas existentes
+            ({platforms} = g);
+            everyPlatforms.push(platforms) 
+        });
+        arrayPlat = [];
+        for (let i = 0; i < everyPlatforms.length; i++) { // recorro todas las plataformas y pusheo sus name a un array
+            everyPlatforms[i].map((p) => {
+                arrayPlat.push(p)
+            })
+        }
+        const names = [...new Set(arrayPlat)] // hago un new Set a el array de names para que no se repitan y lo envio
+        res.status(200).json(names) 
+    }
+    catch (error) {
+        console.log(error)
+    }
+})
+
+router.get('/createGenre', async (req,res) =>{
+    const {name} = req.body;
+    try {
+        const newGenre = await Genre.findOrCreate({
+            where: {name: name}
+        })
+        res.status(201).json(newGenre);
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 module.exports = router;
