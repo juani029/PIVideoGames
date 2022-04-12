a181767383d7478f298451b3659de6f018741b37
Juani <juanignacio0298@gmail.com>
2022-04-12 18:15:50 -0300

    update

--- client/src/components/GameDetail.jsx
+++ client/src/components/GameDetail.jsx
@@ -3,6 +3,7 @@ import { Link, useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getDetail } from "../actions";
+import style from "./styles/GameDetail.module.css";
 
 export default function GameDetail() {
   const dispatch = useDispatch();
@@ -13,42 +14,48 @@ export default function GameDetail() {
   const details = useSelector((state) => state.detail);
 
   return (
-    <div>
+    <div className={style.div}>
       {details.length > 0 ? (
-        <div>
-          <img
-            src={details[0]?.background_image}
-            alt="Not Found"
-            height="500px"
-            width="500px"
-          />
-          <h1>{details[0]?.name}</h1>
-          <div>
-            <p>GENRES</p>
-            {details[0]?.genres?.map((gen) =>
-              gen?.name ? (
-                <h2 key={gen.name}>{gen.name + " "}</h2>
-              ) : (
-                <h2 key={gen}>{gen + " "}</h2>
-              )
-            )}
+        <div className={style.detailDiv}>
+          <div className={style.img}>
+            <img
+              src={details[0]?.background_image}
+              alt="Not Found"
+              height="500px"
+              width="500px"
+            />
+            <h1>{details[0]?.name}</h1>
           </div>
-          <div>
-            <div>
+          <div className={style.box1}>
+            <div className={style.boxChild}>
+              <p>GENRES</p>
+              {details[0]?.genres?.map((gen) =>
+                gen?.name ? (
+                  <h2 key={gen.name}>{gen.name + " "}</h2>
+                ) : (
+                  <h2 key={gen}>{gen + " "}</h2>
+                )
+              )}
+            </div>
+            <div className={style.boxChild}>
               <p>RELEASED</p>
               <h2>{details[0]?.released}</h2>
             </div>
-            <div>
+            <div className={style.boxChild}>
               <p>RATING</p>
               <h2>{details[0]?.rating}</h2>
             </div>
-            <div>
+          </div>
+          <div className={style.box2}>
+            <div className={style.boxChild}>
               <p>PLATFORMS</p>
               {details[0].platforms.map((p) => (
                 <h2 key={p}>{p}</h2>
               ))}
             </div>
-            <div>
+          </div>
+          <div className={style.box3}>
+            <div className={style.boxChild}>
               <p>DESCRIPTION</p>
               <h2
                 dangerouslySetInnerHTML={{
@@ -57,15 +64,16 @@ export default function GameDetail() {
               ></h2>
             </div>
           </div>
+          {/* </div> */}
           <Link to="/home">
-            <button>BACK HOME</button>
+            <button className={style.btn}>BACK HOME</button>
           </Link>
         </div>
       ) : (
         <div>
           <h1>Game not found</h1>
           <Link to="/home">
-            <button>BACK HOME</button>
+            <button className={style.btn}>BACK HOME</button>
           </Link>
         </div>
       )}
